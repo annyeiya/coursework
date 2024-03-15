e499b4e5cd3cc57c9eb4afcb9a88cdc14dbd89aa
root <root@DESKTOP-6LHKFVG>
2024-03-15 21:06:57 +0500

    v1.0

--- main.dart
+++ main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/page/MABPage.dart';
+import 'package:untitled/page/StartPage.dart';
+import 'package:untitled/page/ThirdAPage.dart';
+import 'package:untitled/page/ThirdBPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,173 +14,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          // primarySwatch: Colors.amber,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 200, 155, 250),
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 23,
+              fontWeight: FontWeight.w500,
+            )
+          ),
         ),
         routes: {
           '/': (context) => const StartPage(),
+          '/third_b': (context) => const ThirdBPage(),
+          '/third_a': (context) => const ThirdAPage(),
+          '/mab': (context) => const MABPage(),
         }
     );
   }
 }
 
-const Map<String, String> imageMap = {
-  '1': 'пригодится',
-  '2': 'монограмма3',
-  '3': 'третье'
-};
-
-class StartPage extends StatefulWidget {
-  const StartPage({super.key});
-  @override
-  State<StartPage> createState() => _StartPage();
-}
-
-class _StartPage extends State<StartPage> {
-
-  String? selectedImage = imageMap.keys.first;
-
-  void  _testMethod() {
-    print("hi");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-          title: const Center (
-              child: Text('Susu Map')
-          ),
-          backgroundColor: Colors.amber,
-      ),
-      body: SingleChildScrollView (
-          child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const SizedBox(height: 40.0),
-                  const Text(
-                    'Hello',
-                    style: TextStyle(fontSize: 24.0),
-                  ),
-                  const SizedBox(height: 40.0),
-                  Row (
-                    children: [
-                      Expanded(
-                        flex: 7,
-                        child: InteractiveViewer(
-                            minScale: 1.0,
-                            maxScale: 5.0,
-                            child: SizedBox(
-                              height: MediaQuery.of(context).size.height * 5 / 8,
-                              child: Image.asset(
-                              'assets/img/${imageMap[selectedImage]}.jpg',
-                              key: UniqueKey(),
-                              fit: BoxFit.contain,
-                            )
-                          ),
-                        ),
-                      ),
-                      Expanded (
-                          flex: 1,
-                          child: DropdownButton<String>(
-                            value:selectedImage,
-                            icon: const Icon(Icons.arrow_downward),
-                            elevation: 16,
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                selectedImage = newValue!;
-                              });
-                            },
-                            items: imageMap.keys.map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                          )
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 50.0),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            _testMethod();
-                          },
-                          child: Text('botton 1'),
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            _testMethod();
-                          },
-                          child: Text('botton 2')
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            _testMethod();
-                          },
-                          child: Text('botton 3')
-                      ),
-                    ],
-                  )
-                ]
-            ),
-          )
-      ),
-    );
-  }
-}
-
-
-// class StatfulImage extends StatefulWidget {
-//   const StatfulImage({super.key});
-//   @override
-//   State<StatfulImage> createState() => _StatfulImage();
-// }
-// class _StatfulImage extends State<StatfulImage> {
-//   String? selectedImage = list.first;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//   return Scaffold(
-//     body: Row (
-//       children: [
-//         Expanded(
-//           flex: 7,
-//           child: InteractiveViewer(
-//               minScale: 1.0,
-//               maxScale: 5.0,
-//               child: Image.asset(
-//                 'assets/img/пригодится.jpg',
-//                 fit: BoxFit.contain,
-//               )
-//           ),
-//         ),
-//         Expanded (
-//             flex: 1,
-//             child: DropdownButton<String>(
-//               value:selectedImage,
-//               icon: const Icon(Icons.arrow_downward),
-//               elevation: 16,
-//               onChanged: (String? newValue) {
-//                 setState(() {
-//                   selectedImage = newValue!;
-//                 });
-//               },
-//               items: list.map<DropdownMenuItem<String>>((String value) {
-//                 return DropdownMenuItem<String>(
-//                   value: value,
-//                   child: Text(value),
-//                 );
-//               }).toList(),
-//             )
-//         )
-//       ],
-//     ));
-//   }
-// }
-//
---
+++ page/ThirdBPage.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+const Map<String, String> imageMap = {
+  '1': 'Без_имени',
+  '2': 'пригодится',
+  '3': 'пригодится',
+  '4': 'пригодится',
+  '5': 'пригодится',
+  '6': 'пригодится',
+  '7': 'пригодится',
+  '8': 'пригодится',
+  '9': 'пригодится',
+  '10': 'пригодится',
+  '11': 'пригодится',
+  '12': 'пригодится',
+};
+
+class ThirdBPage extends StatefulWidget {
+  const ThirdBPage({super.key});
+  @override
+  State<ThirdBPage> createState() => _ThirdBPage();
+}
+
+class _ThirdBPage extends State<ThirdBPage> {
+
+  String? selectedImage = imageMap.keys.first;
+
+  void  _testMethod() {
+    print("hi");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: const Center (
+            child: Text('Карта ЮУрГУ')
+        ),
+      ),
+      body: SingleChildScrollView (
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(height: 40.0),
+                  const Text(
+                    'Вы находитесь в корпусе 3бв',
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                  const SizedBox(height: 40.0),
+                  Row (
+                    children: [
+                      Expanded(
+                        flex: 7,
+                        child: InteractiveViewer(
+                          minScale: 1.0,
+                          maxScale: 5.0,
+                          child: SizedBox(
+                              height: MediaQuery.of(context).size.height * 5 / 8,
+                              child: Image.asset(
+                                'assets/img/${imageMap[selectedImage]}.jpg',
+                                key: UniqueKey(),
+                                fit: BoxFit.contain,
+                              )
+                          ),
+                        ),
+                      ),
+                      Expanded (
+                          flex: 1,
+                          child: DropdownButton<String>(
+                            value:selectedImage,
+                            icon: const Icon(Icons.arrow_downward),
+                            elevation: 16,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                selectedImage = newValue!;
+                              });
+                            },
+                            items: imageMap.keys.map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          )
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 50.0),
+                  Row (
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pushReplacementNamed('/mab');
+                        },
+                        child: Text('ГУК'),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            _testMethod();
+                          },
+                          child: Text('корпус 2')
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            _testMethod();
+                          },
+                          child: Text('ЛК')
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pushReplacementNamed('/third_a');
+                          },
+                          child: Text('корпус 3а')
+                      ),
+                    ]),
+                ]
+            ),
+          )
+      ),
+    );
+  }
+}
